17e18a20f077f97fdb666b5701725ad51195759f
Fabian Romero <fabromero@yahoo.com>
2017-03-07 14:59:58 -0300

    Partial commit. Recipe with docker repo and docker-engine installed

---
+++ cookbooks/kubernetes-install/recipes/default.rb
@@ -0,0 +1,16 @@
+package ['apt-transport-https', 'ca-certificates', 'linux-image-extra-$(uname -r)', 'linux-image-extra-virtual']
+
+apt_repository "docker" do
+  uri "https://apt.dockerproject.org/repo"
+  distribution "#{node['platform']}-#{node['lsb']['codename']}"
+  components ["main"]
+  key "https://apt.dockerproject.org/gpg"
+end
+
+#Buscar otra manera de refrescar el apt-cache
+# execute "apt-get-update" do
+#   command "apt-get update"
+# end
+
+package ['docker-engine', 'bridge-utils']
+

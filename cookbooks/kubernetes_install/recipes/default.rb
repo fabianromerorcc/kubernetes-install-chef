b6f11a5b371be482b5d1cb3cbcb62bee1ade0dc6
Fabian Romero <fabromero@yahoo.com>
2017-03-08 18:07:37 -0300

    Added templates for cluster config

--- cookbooks/kubernetes_install/recipes/default.rb
+++ cookbooks/kubernetes_install/recipes/default.rb
@@ -29,7 +29,59 @@ directory '/media/storage'
 
 
 #I didn't find a way to autocreate target_dir if doesn't exist
-tar_extract "https://github.com/GoogleCloudPlatform/kubernetes/releases/download/v1.5.1/kubernetes.tar.gz" do
+tar_extract 'https://github.com/GoogleCloudPlatform/kubernetes/releases/download/v1.5.1/kubernetes.tar.gz' do
   target_dir '/media/storage/'
-  creates '/media/storage/extracted'
+end
+
+tar_extract '/media/storage/kubernetes/server/kubernetes-salt.tar.gz' do
+  action :extract_local
+  target_dir '/media/storage/kubernetes/cluster'
+  creates '/media/storage/kubernetes/cluster/saltbase'
+  tar_flags ['--strip-components 1']
+end
+
+hostname = `hostname -i |awk '{print $1}'`.chomp
+
+
+template '/media/storage/kubernetes/cluster/addons/dns/skydns-rc.yaml.sed' do
+  source 'skydns-rc.yaml.sed.erb'
+  variables ({
+    :hostname  => hostname
+  })
+end
+
+template '/media/storage/kubernetes/cluster/addons/dashboard/dashboard-controller.yaml' do
+  source 'dashboard-controller.yaml.erb'
+  variables ({
+    :hostname  => hostname
+  })
+end
+
+template '/media/storage/kubernetes/cluster/ubuntu/util.sh' do
+  source 'util.sh.erb'
+end
+
+#Check template again when installing in definitive environment
+template '/media/storage/kubernetes/cluster/ubuntu/config-default.sh' do
+  source 'config-default.sh.erb'
+end
+
+#TODO replace only_if with a service validation
+execute 'start kube install' do
+  command 'KUBERNETES_PROVIDER=ubuntu ./kube-up.sh'
+  cwd '//media/storage/kubernetes/cluster'
+  only_if {`ps cax |grep kube |wc -l`.to_i == 0}
+end
+
+# kubectl_path = '/media/storage/kubernetes/cluster/ubuntu/binaries/kubectl'
+# if File.exists?(kubectl_path)
+#   file '/usr/local/bin/kubectl' do
+#     content IO.read(kubectl_path)
+#     mode '775'
+#     # only_if {File.exist?('/media/storage/kubernetes/cluster/ubuntu/binaries/kubectl')}
+#   end
+# end
+#
+link '/usr/local/bin/kubectl' do
+  to '/media/storage/kubernetes/cluster/ubuntu/binaries/kubectl'
 end
